88bd965dd67b4a57b5eaa0a959812c7a15da0e8f
Higor Rocha <higor_36rocha@hotmail.com>
2024-08-14 11:50:49 -0300

    Dimension update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center gap-10 h-dvh sm:h-full">
       <div className="bg-[url('../../public/logo.svg')] h-7 w-[90px] bg-no-repeat mt-10 sm:mt-20"></div>
-      <div className="flex flex-col items-center gap-4 sm:gap-8 sm:mb-10">
+      <div className="flex flex-col items-center gap-8 mb-5 sm:mb-10">
         <Tittle />
         <Form />
       </div>
